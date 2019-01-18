4caabdca28388b2c778397692edd85e0a7504b87
wwwyp <wyp@dc123.com.cn>
2019-01-18 10:47:00 +0800

    Initial commit

---
+++ qmuidemo/src/main/java/com/qmuiteam/qmuidemo/fragment/components/viewpager/QDFitSystemWindowViewPagerFragment.java
@@ -0,0 +1,204 @@
+/*
+ * Tencent is pleased to support the open source community by making QMUI_Android available.
+ *
+ * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the MIT License (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.qmuiteam.qmuidemo.fragment.components.viewpager;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.qmuiteam.qmui.util.QMUIResHelper;
+import com.qmuiteam.qmui.widget.QMUIPagerAdapter;
+import com.qmuiteam.qmui.widget.QMUITabSegment;
+import com.qmuiteam.qmui.widget.QMUIViewPager;
+import com.qmuiteam.qmuidemo.R;
+import com.qmuiteam.qmuidemo.base.BaseFragment;
+import com.qmuiteam.qmuidemo.fragment.QDAboutFragment;
+import com.qmuiteam.qmuidemo.fragment.components.QDButtonFragment;
+import com.qmuiteam.qmuidemo.fragment.components.QDCollapsingTopBarLayoutFragment;
+import com.qmuiteam.qmuidemo.fragment.components.QDTabSegmentFragment;
+import com.qmuiteam.qmuidemo.fragment.components.QDTabSegmentScrollableModeFragment;
+import com.qmuiteam.qmuidemo.lib.annotation.Widget;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * @author cginechen
+ * @date 2017-09-13
+ */
+
+@Widget(name = "QDFitSystemWindowViewPagerFragment")
+public class QDFitSystemWindowViewPagerFragment extends BaseFragment {
+
+    @BindView(R.id.pager) QMUIViewPager mViewPager;
+    @BindView(R.id.tabs) QMUITabSegment mTabSegment;
+
+
+    @Override
+    protected View onCreateView() {
+        FrameLayout layout = (FrameLayout) LayoutInflater.from(getActivity()).inflate(R.layout.fragment_fsw_viewpager, null);
+        ButterKnife.bind(this, layout);
+        initTabs();
+        initPagers();
+        return layout;
+    }
+
+    private void initTabs() {
+        int normalColor = QMUIResHelper.getAttrColor(getActivity(), R.attr.qmui_config_color_gray_6);
+        int selectColor = QMUIResHelper.getAttrColor(getActivity(), R.attr.qmui_config_color_blue);
+        mTabSegment.setDefaultNormalColor(normalColor);
+        mTabSegment.setDefaultSelectedColor(selectColor);
+    }
+
+    private void initPagers() {
+        QMUIPagerAdapter pagerAdapter = new QMUIPagerAdapter() {
+            private FragmentTransaction mCurrentTransaction;
+            private Fragment mCurrentPrimaryItem = null;
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == ((Fragment) object).getView();
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "TabSegment";
+                    case 1:
+                        return "CollapsingTopBar";
+                    case 2:
+                        return "About";
+                    case 3:
+                    default:
+                        return "ViewPager";
+                }
+            }
+
+            @SuppressLint("CommitTransaction")
+            @Override
+            protected Object hydrate(ViewGroup container, int position) {
+                String name = makeFragmentName(container.getId(), position);
+                if (mCurrentTransaction == null) {
+                    mCurrentTransaction = getChildFragmentManager()
+                            .beginTransaction();
+                }
+                Fragment fragment = getChildFragmentManager().findFragmentByTag(name);
+                if(fragment != null){
+                    return fragment;
+                }
+                switch (position) {
+                    case 0:
+                        return new QDTabSegmentScrollableModeFragment();
+                    case 1:
+                        return new QDCollapsingTopBarLayoutFragment();
+                    case 2:
+                        return new QDAboutFragment();
+                    case 3:
+                    default:
+                        return new QDViewPagerFragment();
+                }
+            }
+
+            @SuppressLint("CommitTransaction")
+            @Override
+            protected void populate(ViewGroup container, Object item, int position) {
+                String name = makeFragmentName(container.getId(), position);
+                if (mCurrentTransaction == null) {
+                    mCurrentTransaction = getChildFragmentManager()
+                            .beginTransaction();
+                }
+                Fragment fragment = getChildFragmentManager().findFragmentByTag(name);
+                if (fragment != null) {
+                    mCurrentTransaction.attach(fragment);
+                    if(fragment.getView() != null && fragment.getView().getWidth() == 0){
+                        fragment.getView().requestLayout();
+                    }
+                } else {
+                    fragment = (Fragment) item;
+                    mCurrentTransaction.add(container.getId(), fragment, name);
+                }
+                if (fragment != mCurrentPrimaryItem) {
+                    fragment.setMenuVisibility(false);
+                    fragment.setUserVisibleHint(false);
+                }
+            }
+
+            @SuppressLint("CommitTransaction")
+            @Override
+            protected void destroy(ViewGroup container, int position, Object object) {
+                if (mCurrentTransaction == null) {
+                    mCurrentTransaction = getChildFragmentManager()
+                            .beginTransaction();
+                }
+                mCurrentTransaction.detach((Fragment) object);
+            }
+
+            @Override
+            public void startUpdate(ViewGroup container) {
+                if (container.getId() == View.NO_ID) {
+                    throw new IllegalStateException("ViewPager with adapter " + this
+                            + " requires a view id");
+                }
+            }
+
+            @Override
+            public void finishUpdate(ViewGroup container) {
+                if (mCurrentTransaction != null) {
+                    mCurrentTransaction.commitNowAllowingStateLoss();
+                    mCurrentTransaction = null;
+                }
+            }
+
+            @Override
+            public void setPrimaryItem(ViewGroup container, int position, Object object) {
+                Fragment fragment = (Fragment) object;
+                if (fragment != mCurrentPrimaryItem) {
+                    if (mCurrentPrimaryItem != null) {
+                        mCurrentPrimaryItem.setMenuVisibility(false);
+                        mCurrentPrimaryItem.setUserVisibleHint(false);
+                    }
+                    if (fragment != null) {
+                        fragment.setMenuVisibility(true);
+                        fragment.setUserVisibleHint(true);
+                    }
+                    mCurrentPrimaryItem = fragment;
+                }
+            }
+
+            private String makeFragmentName(int viewId, long id) {
+                return "QDFitSystemWindowViewPagerFragment:" + viewId + ":" + id;
+            }
+        };
+        mViewPager.setAdapter(pagerAdapter);
+        mTabSegment.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    protected boolean canDragBack() {
+        return mViewPager.getCurrentItem() == 0;
+    }
+}
